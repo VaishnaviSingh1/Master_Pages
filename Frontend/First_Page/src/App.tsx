13422c851fb18cdb6717c953f51e8efe1f2ca11a
amitgupt-a <zamit7441@gmail.com>
2025-06-18 15:13:20 +0530

    Header

--- Frontend/First_Page/src/App.tsx
+++ Frontend/First_Page/src/App.tsx
@@ -1,11 +1,16 @@
 // import { Button } from "../components/ui/button"
+import Header from "./components/Header"
 import { Button } from "./components/ui/button"
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
+      <Header/>
     </div>
+    
+    
+
   )
 }
 
